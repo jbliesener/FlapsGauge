8753992b6cf602fafc91e2f7dfa3e75b2691a6a0
Jorg Neves Bliesener <jbliesener@bliesener.com>
2023-07-30 13:45:28 +0200

    Update FlightSimGauge.h

--- include/FlightSimGauge.h
+++ include/FlightSimGauge.h
@@ -1,148 +1,48 @@
-#include "FlightSimGauge.h"
+#include <Arduino.h>
+#include <AccelStepper.h>
 
-FlightSimGauge::FlightSimGauge(const char* gaugeName, AccelStepper &gaugeStepper) :
-    name(gaugeName),
-    stepper(gaugeStepper)
- {
-    mode=GAUGE_OPERATING;
-    timer = 0;
-    debugMode = false;
-    manualHomingPin = -1;
-    autoHomingPin = -1;
-    isLimitHoming = false;
-    homingLevel = LOW;
-    manualHomingPressDelay = 100;
-    manualHomingReleaseDelay = 1000;
-    homingPosition = 0;
-}
+class FlightSimGauge {
+    public:
+        enum FlightSimGaugeMode {
+            GAUGE_OPERATING,        
+            GAUGE_HOMING_WAIT_PRESS,
+            GAUGE_HOMING_MANUAL,
+            GAUGE_HOMING_WAIT_RELEASE,
+            GAUGE_HOMING_AUTO,
+            GAUGE_HOMING_LIMIT
+        };
 
-void FlightSimGauge::setManualHomingPinAndSpeed(int16_t pinNumber, uint8_t activeLevel, int32_t homingSpeed) {
-    manualHomingPin = pinNumber;
-    homingLevel = activeLevel;
-    this->homingSpeed = homingSpeed;
-    autoHomingPin = -1;
-    isLimitHoming = false;
-    mode=GAUGE_OPERATING;
-    if (activeLevel == LOW) {
-        pinMode(pinNumber, INPUT_PULLUP);
-    } else if (activeLevel == HIGH) {
-        pinMode(pinNumber, INPUT_PULLDOWN);
-    }
-}
+        FlightSimGauge(const char *gaugeName, AccelStepper &stepper);
+        void setManualHomingPinAndSpeed(int16_t pinNumber, uint8_t activeLevel, int32_t homingSpeed);
+        void setManualHomingPinTiming(uint32_t pressDelay, uint32_t releaseDelay);
+        void setAutoHomingPinAndSpeed(int16_t pinNumber, uint8_t activeLevel, int32_t homingSpeed);
+        void setLimitHoming(int32_t steps);
+        void setHomingPosition(int32_t position) { this->homingPosition = homingPosition; }
+        void setTargetPosition(int32_t targetPosition) { if (mode == GAUGE_OPERATING) stepper.moveTo(targetPosition); };
 
-void FlightSimGauge::setManualHomingPinTiming(uint32_t pressDelay, uint32_t releaseDelay) {
-    manualHomingPressDelay = pressDelay;
-    manualHomingReleaseDelay = releaseDelay;
-}
+        void loop();
 
-void FlightSimGauge::setAutoHomingPinAndSpeed(int16_t pinNumber, uint8_t activeLevel, int32_t homingSpeed) {
-    autoHomingPin = pinNumber;
-    homingLevel = activeLevel;
-    manualHomingPin = -1;
-    isLimitHoming = false;
-    mode=GAUGE_HOMING_AUTO;
-    stepper.setSpeed(homingSpeed);
-    if (activeLevel == LOW) {
-        pinMode(pinNumber, INPUT_PULLUP);
-    } else if (activeLevel == HIGH) {
-        pinMode(pinNumber, INPUT_PULLDOWN);
-    }
-}
+        void setDebug(bool debugMode, uint32_t timer=500) { this->debugMode = debugMode; debugInterval=timer;  };
+        FlightSimGaugeMode getMode() { return mode; };
 
-void FlightSimGauge::setLimitHoming(int32_t steps) {
-    manualHomingPin = -1;
-    autoHomingPin = -1;
-    isLimitHoming = true;
-    mode = GAUGE_HOMING_LIMIT;
+    private:
+        const char *name;
+        AccelStepper &stepper;
+        FlightSimGaugeMode mode;
+        elapsedMillis timer;
+        elapsedMillis debugTimer;
+        uint32_t debugInterval;
+        bool debugMode;
+        int16_t manualHomingPin;
+        int16_t autoHomingPin;
+        bool isLimitHoming;
+        int32_t homingSpeed;
+        uint8_t homingLevel;
+        int32_t homingPosition;
+        uint32_t manualHomingPressDelay;
+        uint32_t manualHomingReleaseDelay;
+        int32_t limitHomingPosition;
 
-    limitHomingPosition = steps;   
-    stepper.moveTo(limitHomingPosition);
-}
-
-void FlightSimGauge::loop() {
-    switch (mode) {
-        case GAUGE_OPERATING: 
-            stepper.run();
-            if (manualHomingPin != -1) {
-                if (digitalReadFast(manualHomingPin) == homingLevel) {
-                    setMode(GAUGE_HOMING_WAIT_PRESS);
-                    timer = 0;
-                }
-            }
-            break;
-
-        case GAUGE_HOMING_WAIT_PRESS:
-            if (digitalReadFast(manualHomingPin) != homingLevel) {
-                setMode(GAUGE_OPERATING);
-                break;
-            }
-            if (timer > manualHomingPressDelay) {
-                setMode(GAUGE_HOMING_MANUAL);
-                stepper.setSpeed(homingSpeed);
-            }
-            break;
-
-        case GAUGE_HOMING_MANUAL:
-            if (digitalReadFast(manualHomingPin) != homingLevel) {
-                timer = 0;
-                setMode(GAUGE_HOMING_WAIT_RELEASE);
-                break;
-            }
-            stepper.runSpeed();
-            break;
-
-        case GAUGE_HOMING_WAIT_RELEASE:
-            if (digitalReadFast(manualHomingPin) == homingLevel) {
-                setMode(GAUGE_HOMING_MANUAL);
-            }
-            if (timer > manualHomingReleaseDelay) {
-                stepper.setCurrentPosition(homingPosition);
-                setMode(GAUGE_OPERATING);
-            }
-            break;
-
-        case GAUGE_HOMING_AUTO:
-            if (digitalReadFast(autoHomingPin) == homingLevel) {
-                stepper.setCurrentPosition(homingPosition);
-                setMode(GAUGE_OPERATING);
-                break;
-            }
-            stepper.runSpeed();
-            break;
-
-        case GAUGE_HOMING_LIMIT:
-            if (stepper.currentPosition() == limitHomingPosition) {
-                stepper.setCurrentPosition(homingPosition);
-                setMode(GAUGE_OPERATING);
-                break;
-            }
-            stepper.runSpeed();
-            break;
-    }
-    if (debugMode && debugTimer > debugInterval) {
-        debugTimer = 0;
-        Serial.printf("%10lu: Gauge %s, mode %s, current position: %d, target position %d\n", millis(), 
-            name, getModeString(mode),
-            stepper.currentPosition(), stepper.targetPosition());
-    }
- }
-
-const char *FlightSimGauge::getModeString(FlightSimGaugeMode mode) {
-    switch (mode) {
-        case GAUGE_OPERATING:           return "OPERATING";           break;
-        case GAUGE_HOMING_WAIT_PRESS:   return "HOMING_WAIT_PRESS";   break;
-        case GAUGE_HOMING_MANUAL:       return "HOMING_MANUAL";       break;
-        case GAUGE_HOMING_WAIT_RELEASE: return "HOMING_WAIT_RELEASE"; break;
-        case GAUGE_HOMING_AUTO:         return "HOMING_AUTO";         break;
-        case GAUGE_HOMING_LIMIT:        return "HOMING_LIMIT";        break;
-        default: return "UNKNOWN"; break;
-    }
-}
-
-void FlightSimGauge::setMode(FlightSimGaugeMode mode) {
-    if (debugMode) {
-        Serial.printf("%10lu: Switching gauge %s from mode %s to mode %s\n", millis(), 
-        name, getModeString(this->mode),getModeString(mode));
-    }
-    this->mode = mode;
-}
+        const char *getModeString(FlightSimGaugeMode mode);
+        void setMode(FlightSimGaugeMode mode);
+};
